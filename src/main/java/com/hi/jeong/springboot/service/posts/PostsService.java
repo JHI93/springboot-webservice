8822044ac593a9953d3c43a748a119f03c61c8b5
JeongHaeIn <wjdgodls0827@naver.com>
2020-05-28 23:25:00 +0900

    [ADD] function SELECT

--- src/main/java/com/hi/jeong/springboot/service/posts/PostsService.java
+++ src/main/java/com/hi/jeong/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.hi.jeong.springboot.service.posts;
 
 import com.hi.jeong.springboot.domain.posts.Posts;
 import com.hi.jeong.springboot.domain.posts.PostsRepository;
+import com.hi.jeong.springboot.web.dto.PostsListResponseDto;
 import com.hi.jeong.springboot.web.dto.PostsResponseDto;
 import com.hi.jeong.springboot.web.dto.PostsSaveRequestDto;
 import com.hi.jeong.springboot.web.dto.PostsUpdateRequestsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/hi/jeong/springboot/web/IndexController.java
+++ src/main/java/com/hi/jeong/springboot/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.hi.jeong.springboot.web;
 
+import com.hi.jeong.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.websocket.server.ServerEndpoint;
@@ -10,13 +12,16 @@ import javax.websocket.server.ServerEndpoint;
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+    return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 }
