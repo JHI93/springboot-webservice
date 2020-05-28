3f62afbcb3bdaf91a886cb6fcbe54566943a01ed
JeongHaeIn <wjdgodls0827@naver.com>
2020-05-28 22:27:24 +0900

    [ADD] function POST

--- src/main/java/com/hi/jeong/springboot/web/IndexController.java
+++ src/main/java/com/hi/jeong/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.hi.jeong.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.websocket.server.ServerEndpoint;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
